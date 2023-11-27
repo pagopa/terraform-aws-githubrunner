a16ff420fe03740a64cd287cba41d5c932d916ef
trossibip <tommaso.rossi@bip-group.com>
2023-11-27 15:17:45 +0100

    fix: Add policy in example for reading s3 buckets

--- examples/simple/50-githubrunner.tf
+++ examples/simple/50-githubrunner.tf
@@ -2,6 +2,24 @@ locals {
   ecs_cluster_name = "ghrunner-cluster"
 }
 
+resource "aws_iam_policy" "list_buckets" {
+  name        = "ListBuckets"
+  description = "Policy for listing s3 buckets"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:List*",
+        ],
+        Resource = ["*"],
+      },
+    ]
+  })
+}
+
 module "aws_githubrunner" {
   source = "../.."
 
@@ -11,4 +29,5 @@ module "aws_githubrunner" {
   ecs_create_cluster = true
   vpc_id             = module.vpc.vpc_id
   github_repository  = "pagopa/terraform-aws-githubrunner"
+  iam_policy_arns    = [aws_iam_policy.list_buckets.arn]
 }
